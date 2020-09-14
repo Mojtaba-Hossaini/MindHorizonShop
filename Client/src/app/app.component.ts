e02f542a515e5b5fabe94655098a5ae954e4073f
Mojtaba <s.mojtaba1987@gmail.com>
2020-09-14 09:57:25 +0430

    add http client

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'MindHorizon';
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products').subscribe((result: any) => {
+      console.log(result);
+    }, error => {
+      console.log(error);
+    });
+  }
 }
