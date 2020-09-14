87869623b833a7b9bb2edd3af0b4b1a2ae1fe3e2
Mojtaba <s.mojtaba1987@gmail.com>
2020-09-14 12:26:19 +0430

    add interface for product client 24.6.1399

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { IProduct } from './models/product';
+import { IPagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -8,12 +10,13 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
   title = 'MindHorizon';
+  products: IProduct[];
 
   constructor(private http: HttpClient){}
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products').subscribe((result: any) => {
-      console.log(result);
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((result: IPagination) => {
+      this.products = result.data;
     }, error => {
       console.log(error);
     });
